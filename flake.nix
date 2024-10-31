7595473a4b8e965184cd981b60adab8f38b1431b
Luna Heyman <luna.heyman@proton.me>
2024-10-31 17:07:10 +0100

    feat: add basic desktop configuration

--- flake.nix
+++ flake.nix
@@ -10,5 +10,10 @@
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
       formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
+
+      nixosConfigurations.luna-desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/luna-desktop/configuration.nix ];
+      };
     };
 }
---
+++ hosts/luna-desktop/configuration.nix
@@ -0,0 +1,105 @@
+{ pkgs, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  system.stateVersion = "24.05";
+  nix.settings.experimental-features = "nix-command flakes";
+  nixpkgs.config.allowUnfree = true;
+
+  time.timeZone = "Europe/Brussels";
+  i18n.defaultLocale = "en_US.UTF-8";
+  security.sudo.extraConfig = "Defaults env_reset,pwfeedback";
+  console.useXkbConfig = true;
+
+  boot = {
+    initrd.kernelModules = [ "amdgpu" ];
+
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = "luna-desktop";
+    firewall.enable = true;
+  };
+
+  services.displayManager.ly = {
+    enable = true;
+
+    settings = {
+      vi_mode = true;
+      clear_password = true;
+      clock = "%D %H:%M";
+    };
+  };
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "amdgpu" ];
+
+    xkb = {
+      layout = "us";
+      options = "caps:escape";
+    };
+  };
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  programs.fish = {
+    enable = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+    gamescopeSession.enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  users.users.luna = {
+    isNormalUser = true;
+    description = "Luna Heyman";
+    extraGroups = [ "wheel" ];
+    shell = pkgs.fish;
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = 1;
+    EDITOR = "nvim";
+    BROWSER = "firefox";
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    firefox
+    alacritty
+    rofi-wayland
+    spotify
+    discord
+    hyprpaper
+    ncpamixer
+    stremio
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Iosevka" ]; })
+  ];
+}
